77d7a963d73279c7c1c8231a1812064abd464c32
unknown <madcat182@gmail.com>
2018-04-30 11:51:22 -0500

    General update

--- Scripts/00 ThemeInfo.lua
+++
@@ -1,3 +0,0 @@
-themeVersion = "Alpha 1"
-themeDate = "2011/09/03 15:40"
-ThemeDir = THEME:GetCurrentThemeDirectory()
--- Scripts/SortHandler.lua
+++
@@ -1,4 +0,0 @@
-Sort5thCS = function()
-	SONGMAN:SetPreferredSongs("5thCS")
-	return "sort,Preferred";
-end
